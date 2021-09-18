6d8b558e9c66e1cc7cad10ffc2f243aabf567ddc
Eliane Cristina <eliane.cristina18@icloud.com>
2021-09-17 22:35:48 -0300

    adicionando exercicios anteriores

--- quinzena1/aula03/index.js
+++ quinzena1/aula03/index.js
@@ -1,26 +1,4 @@
-/* 
-### EXERCÍCIO 1
+const nome = "matheus"
+conts frase = " meu nome é " + nome ;
 
-No primeiro console.log o valor impresso vai ser 10
-No segundo console.log o valor imporesso vai ser 10 5
-
-### EXERCÍCIO 2
-
-No console.log vai aparecer 10 10 10 
-
-### EXERCÍCIO 3
-
-let horasPorDia = prompt("Quantas horas você trabalha por dia?")
-let valorPorDia = prompt("Quanto você recebe por dia?")
-alert(`Voce recebe ${valorPorDia/horasPorDia} por hora`) **/
-
-/* No typeof de nomes ele retornou uma string 
-e no de iade retornou o tipo number */
-
-let nome = prompt("Qual seu nome?")
-let idade = prompt("Quantos anos você tem?")
-alert(`Oi, ${nome}, você tem ${idade} anos`) 
-
-console.log(`Oi, ${nome}, você tem ${idade} anos`)
-
-/* No console.log retournou string e o número adicionado no prompt */
+console.log(frase)
